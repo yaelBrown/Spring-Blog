dc5e8b7296576d7785f3d82d3c676641a7be5e0a
yaelBrown <yaelrbrown@gmail.com>
2019-11-09 15:09:01 -0600

    Able to run other methods in SpringBlogApplication class.

--- src/main/java/com/example/demo/SpringBlogApplication.java
+++ src/main/java/com/example/demo/SpringBlogApplication.java
@@ -3,11 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.api.ApiCall;
+
+
 @SpringBootApplication
 public class SpringBlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBlogApplication.class, args);
+
+        ApiCall a = new ApiCall();
+
+        System.out.println(a.getApiCall());
     }
 
 }
---
+++ src/main/java/com/example/demo/api/ApiCall.java
@@ -0,0 +1,10 @@
+package com.example.demo.api;
+
+public class ApiCall {
+
+    private String apiCall = "This is a call to the api";
+
+    public String getApiCall() {
+        return apiCall;
+    }
+}
